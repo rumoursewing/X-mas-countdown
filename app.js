01380a15fa2d16694c4d78f212942e3147b7ac09
root <root@A5-004006.localdomain>
2018-11-28 17:18:03 -0700

    starter code

---
+++ app.js
@@ -0,0 +1,9 @@
+// Countdown to dec 25
+
+//html element grabber
+// the date for today
+//the date we want (dec 25)
+//calculation, to figure out the time difference in the time
+//display on the html 
+
+// eventlistener, onchange, the change of the difference calculation
